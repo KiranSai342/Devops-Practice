94dc1736f4264e24debf2aa4ac507ee5a6f31b91
Kiran <saikiranvinnakota059@gmail.com>
2024-08-27 20:14:10 +0530

    Datatypes

--- Datatypes.sh
+++ Datatypes.sh
@@ -3,7 +3,7 @@
 Number1=$1
 Number2=$2
 
-Sum=(($Number1+$Number2))
+Sum=$(($Number1+$Number2))
 #Outer braces for taking the avlue in runtime and assigning it it Sum
 #Inner Braces for adding function (+)
 
