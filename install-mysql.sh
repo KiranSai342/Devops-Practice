bded84457eca802f2cf8d9d7988e3c5531dced34
Kiran <saikiranvinnakota059@gmail.com>
2024-08-28 00:04:11 +0530

    Install MySql

--- install-mysql.sh
+++ install-mysql.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-Userid=$(sudo id -u)
+Userid=$(id -u)
 
 if [ $Userid -ne 0 ]
 then
     echo "please run the script with root previllages"
     exit 1
 fi
-sudo dnf install mysql-server -y
+dnf install mysql-server -y
+
+
+
+##for the user not having root access
+#Userid=$(sudo id -u)
+
+#if [ $Userid -ne 0 ]
+#then
+    #echo "you are not having root user access please contact your system administrator."
+    #exit 1
+#fi
+#sudo dnf install mysql-server -y
+
 
