9787cec176171afa1647b56eb58c950aa2f3196a
Kiran <saikiranvinnakota059@gmail.com>
2024-08-27 23:25:08 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -6,6 +6,7 @@ if [ $Number -gt 20 ] #-gt = Greatet then, -lt = Less then, -eq = equals, -ne = 
 then
     echo "The given Number: $Number is Greater then : 20"
 elif [ $Number -eq 20 ]
+then
     echo "The given Number: $Number is equals to : 20"
 else
     echo "The given Number: $Number is Less then : 20"
