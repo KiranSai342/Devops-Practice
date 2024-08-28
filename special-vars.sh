5f93bfbe5d8d475b860f9cb2405e820b24c92efa
Kiran <saikiranvinnakota059@gmail.com>
2024-08-28 23:01:29 +0530

    Special Variables

--- special-vars.sh
+++ special-vars.sh
@@ -6,6 +6,7 @@ echo "Script Name: $0"
 echo "Current working Directory: $PWD"
 echo "Home directory of current user : $HOME"
 echo "PID of the current running script: $$"
+echo "Exit status of the lst command : $?"
 
 sleep 100 &
 
