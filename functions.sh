c7384454df5398b7ae06cf9135ec7d9b5d1bd999
Kiran <saikiranvinnakota059@gmail.com>
2024-08-28 22:27:58 +0530

    Functions

---
+++ functions.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+Userid=$(id -u)
+
+Validate(){
+    echo "exit status $1"
+}
+
+if [ $Userid -ne 0 ]
+then
+    echo "please run the script with root previllages"
+    exit 1
+fi
+dnf list installed mysql-server
+
+Validate $?
+
+
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Mysql is not installed. Going to install it."
+#     dnf install mysql-server -y
+#     if [ $? -ne 0 ]
+#     then    
+#         echo "Installation not successed"
+#         exit 1
+#     else
+#         echo "Installation success"
+#     fi
+# else
+#     echo "Mysql already installed. Nothing to do."
+# fi
+
+
+
+##for the user not having root access
+#Userid=$(sudo id -u)
+
+#if [ $Userid -ne 0 ]
+#then
+    #echo "you are not having root user access please contact your system administrator."
+    #exit 1
+#fi
+#sudo dnf install mysql-server -y
+
+
